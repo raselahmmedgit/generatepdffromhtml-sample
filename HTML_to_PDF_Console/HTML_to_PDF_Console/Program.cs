657638b165cb6bb417e9c6cb721e707f725caf2e
Rasel Ahmmed <raselahmmed@hotmail.com>
2024-07-10 14:24:35 +0600

    2024-07-10

---
+++ HTML_to_PDF_Console/HTML_to_PDF_Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using System.Text.Json.Nodes;
+
+var client = new RestClient("https://localhost:7045/pdf");
+
+string path = Path.GetFullPath("../../../Templates/");
+
+string currentDirectory = Directory.GetCurrentDirectory();
+
+var request = new RestRequest().AddFile("index.html", path + "index.html")
+    .AddFile("style.css", path + "style.css").AddFile("data.json", path + "data.json").AddFile("logo.png", path + "logo.png").AddFile("barcode.png", path + "barcode.png").AddFile("Inter-Regular.ttf", path + "Inter-Regular.ttf");
+
+request.Method = Method.Post;
+
+var json = new JsonObject
+{
+    ["index"] = "index.html",
+    ["data"] = "data.json",
+    ["width"] = 0,
+    ["height"] = 0,
+    ["margin"] = 0,
+    ["assets"] = new JsonArray
+              {
+                "style.css",
+                "logo.png",
+                "barcode.png",
+                "Inter-Regular.ttf"
+              }
+};
+
+request.AddParameter("application/json", json.ToString(), ParameterType.RequestBody);
+
+var response = await client.ExecuteAsync(request);
+
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    System.IO.File.WriteAllBytes("result.pdf", response.RawBytes);
+
+}
